9d07516e38b3912fb945a2afbc9f60b1484bf67d
avestar <avestar121@gmail.com>
2023-06-08 11:35:50 +0200

    small changes

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -10,7 +10,7 @@ const AboutMe = () => {
       <div className='getToKnow'>
         <h3 className='aboutmesmall'>Get to know me!</h3>
         <br/>
-        <p className='aboumePar'>Hi, i&apos;m Aleksey, but you can call me Alex, I am passionate and dedicated full-stack developer. When I&apos;m not coding, you can find me playing videogames, reading books, or working out at the gym. Sport inspires me to think creatively and approach problem-solving from different angles. I&apos;m fluent in three languages: English, Ukrainian, and Russian, and I&apos;m currently studying Polish and German. I am communicative and not afraid to take on tasks of any complexity. If you have a project in mind or would like to collaborate, feel free to<span><a href="#contact" style={{color: "#5867C9"}}> get in touch.</a></span></p>
+        <p className='aboumePar'>Hi, i&apos;m Aleksey, I am passionate and dedicated full-stack developer. When I&apos;m not coding, you can find me playing videogames, reading books, or working out at the gym. Sport inspires me to think creatively and approach problem-solving from different angles. I&apos;m fluent in three languages: English, Ukrainian, and Russian, and I&apos;m currently studying Polish and German. I am communicative and not afraid to take on tasks of any complexity. If you have a project in mind or would like to collaborate, feel free to<span><a href="#contact" style={{color: "#5867C9"}}> get in touch.</a></span></p>
       <br/>
       <p className='aboutMeIdeas'>Let&apos;s work together to bring your ideas to life!</p>
       </div>
