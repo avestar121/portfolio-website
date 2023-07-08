cfc2a64a204d5ca69f01f9854e3aae311e738783
avestar <avestar121@gmail.com>
2023-07-08 17:37:52 +0200

    added animated background

---
+++ components/AnimatedBackground.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const AnimatedBackground = () => {
+  return (
+    <div className="animatedBackground">
+      <div className="squares">
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+      </div>
+    </div>
+  );
+}
+
+export default AnimatedBackground;
--- components/LandingPage.js
+++ components/LandingPage.js
@@ -1,5 +1,7 @@
-'use client'
 import React, { useEffect, useState } from 'react';
+import AnimatedBackground from './AnimatedBackground';
+import '../styles/animated-background.css';
+
 
 const LandingPage = () => {
   const [showText, setShowText] = useState(false);
@@ -7,16 +9,20 @@ const LandingPage = () => {
   useEffect(() => {
     setShowText(true);
   }, []);
+
   return (
-    <div className='landingPage' id="home">
-        <h1 className={`landingHeader ${showText ? 'show' : ''}`}>HI, I&apos;M ALEKSEY, A FULL-STACK DEVELOPER</h1>
-        <div className='socialLinks'>
-          <a href='https://github.com/avestar121'><img src="/github.png" alt="github" className="socialsImage"/></a>
-          <a href='mailto:avestar121@gmail.com'><img src="/envelope.png" alt="mail" className="socialsImage"/></a>
-          <a href='https://www.linkedin.com/in/asotnykov/'><img src="/linkedin.png" alt="github" className="socialsImage"/></a>
+    <div>
+      <AnimatedBackground />
+      <div className={`landingPage ${showText ? 'show' : ''}`} id="home">
+        <h1 className="landingHeader">HI, I'M ALEKSEY, A FULL-STACK DEVELOPER</h1>
+        <div className="socialLinks">
+          <a href="https://github.com/avestar121"><img src="/github.png" alt="github" className="socialsImage"/></a>
+          <a href="mailto:avestar121@gmail.com"><img src="/envelope.png" alt="mail" className="socialsImage"/></a>
+          <a href="https://www.linkedin.com/in/asotnykov/"><img src="/linkedin.png" alt="linkedin" className="socialsImage"/></a>
         </div>
-  </div>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
